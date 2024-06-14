6f267d69ded42bd0e2b5d0af7633277436ab4b8b
100percentBAKA <1rn21cs011.adarshgs@gmail.com>
2024-06-14 20:37:56 +0530

    fix ui

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,137 +1,112 @@
 "use client";
-
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { IoSend } from "react-icons/io5";
 import ReactMarkdown from "react-markdown";
 import { RiRobot2Line } from "react-icons/ri";
-
-interface Message {
-    sender: "human" | "ai";
-    text: string;
-}
+import { useChat } from "@ai-sdk/react";
 
 function Chat() {
-    const [inputValue, setInputValue] = useState<string>("");
-    const [messages, setMessages] = useState<Message[]>([]);
+  const { messages, input, setInput, handleInputChange, handleSubmit } =
+    useChat();
+  const chatContainerRef = useRef<HTMLDivElement>(null);
+  const lastMessageRef = useRef<HTMLDivElement>(null);
 
-    const handleSuggestionClick = (text: string) => {
-        setInputValue(text);
-    };
+  const handleSuggestionClick = (text: string) => {
+    setInput(text);
+  };
 
-    const handleBtnClick = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        if (inputValue.trim() !== "") {
-            const newMessage: Message = { sender: "human", text: inputValue };
-            setMessages([
-                ...messages,
-                newMessage,
-                generateAiResponse(newMessage.text),
-            ]);
-            setInputValue("");
-        }
-    };
+  const suggestions = [
+    "When is Adarsh's birthday? 🎉",
+    "How do I contact you 👋",
+    "Do you have any work experience ⚒️",
+    "What is your github account 👁️",
+  ];
 
-    const generateAiResponse = (humanMessage: string): Message => {
-        const aiResponses: { [key: string]: string } = {
-            "When is Adarsh's birthday? 🎉": "July 1st 🤩",
-            "How do I contact you 👋":
-                "You can contact me via email at example@example.com.",
-            "Do you have any work experience ⚒️":
-                "Yes, I have experience in web development.",
-            "What is your github account 👁️": `
-[100percentBAKA](https://www.github.com/100percentBAKA)
-        `,
-        };
+  useEffect(() => {
+    const chatContainer = chatContainerRef.current;
+    const lastMessage = lastMessageRef.current;
 
-        //! React Markdown has some issues with initial white spaces, remove initial white space beforehand
+    if (chatContainer && lastMessage) {
+      const containerHeight = chatContainer.clientHeight;
+      const messageHeight = lastMessage.offsetTop + lastMessage.clientHeight;
 
-        return {
-            sender: "ai",
-            text: aiResponses[humanMessage] || "I'm not sure about that.",
-        };
-    };
+      if (messageHeight > containerHeight) {
+        chatContainer.scrollTop = messageHeight - containerHeight;
+      }
+    }
+  }, [messages]);
 
-    const suggestions = [
-        "When is Adarsh's birthday? 🎉",
-        "How do I contact you 👋",
-        "Do you have any work experience ⚒️",
-        "What is your github account 👁️",
-    ];
-
-    return (
-        <section className="max-w-7xl w-full h-full flex flex-col px-4 md:px-8 mx-auto">
-            <div>
-                <h1 className="text-4xl font-semibold lg:text-5xl pt-5">Playground</h1>
-                <p className="leading-7 text-muted-foreground mt-2">
-                    Chat with my portfolio!
-                </p>
+  return (
+    <section className="max-w-7xl w-full h-full flex flex-col px-4 md:px-8 mx-auto">
+      <div>
+        <h1 className="text-4xl font-semibold lg:text-5xl pt-5">Playground</h1>
+        <p className="leading-7 text-muted-foreground mt-2">
+          Chat with my portfolio!
+        </p>
+      </div>
+      <div className="rounded-md border border-gray-200 w-full h-full mt-8 flex flex-col">
+        {/* Suggestion Windows */}
+        <div className="grid grid-cols-1 lg:grid-cols-2 items-center p-3 gap-3 border-b border-gray-200">
+          {suggestions.map((text, index) => (
+            <div
+              key={index}
+              className="border border-gray-200 p-2 rounded-md cursor-pointer transition-colors duration-150 hover:border-blue-300"
+              onClick={() => handleSuggestionClick(text)}
+            >
+              {text}
             </div>
-
-            <div className="rounded-md border border-gray-200 w-full h-full mt-8 flex flex-col">
-                {/* Suggestion Windows */}
-                <div className="grid grid-cols-1 lg:grid-cols-2 items-center p-4 gap-6 border-b border-gray-200">
-                    {suggestions.map((text, index) => (
-                        <div
-                            key={index}
-                            className="border border-gray-200 p-2 rounded-md cursor-pointer transition-colors duration-150 hover:border-blue-300"
-                            onClick={() => handleSuggestionClick(text)}
-                        >
-                            {text}
-                        </div>
-                    ))}
-                </div>
-
-                {/* Chat Window */}
-                <div className="flex-1 p-4 overflow-y-auto min-h-[60vh] flex flex-col">
-                    {messages.map((message, index) => (
-                        <div
-                            key={index}
-                            className={`my-2 max-w-xs p-2 rounded-md ${
-                                message.sender === "human"
-                                    ? "bg-blue-100 self-end"
-                                    : "bg-gray-100 self-start"
-                            }`}
-                        >
-                            <ReactMarkdown>{message.text}</ReactMarkdown>
-                        </div>
-                    ))}
-
-                    {/* Fall back for empty chat window */}
-                    <div className="flex-1 min-h-[60vh] w-full flex items-center justify-center">
-                        {messages.length === 0 && (
-                            <div className="flex flex-col space-y-4 items-center">
-                                <RiRobot2Line className="w-[24px] h-[24px]" />
-                                <p>Send a message to start chatting with my portfolio</p>
-                                <p>Feel free to use the above suggestions</p>
-                            </div>
-                        )}
-                    </div>
-                </div>
-
-                {/* Input Field goes here . . . */}
-                <div className="p-4 border-t border-gray-200">
-                    <form
-                        className="flex items-center space-x-2"
-                        onSubmit={handleBtnClick}
-                    >
-                        <Input
-                            type="text"
-                            value={inputValue}
-                            onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setInputValue(e.target.value)}
-                            className="flex-1 border border-gray-300 p-2 rounded-md"
-                            placeholder="Type a message..."
-                        />
-                        <Button className="rounded-md flex items-center" type="submit">
-                            <span className="hidden lg:flex">Send</span>
-                            <IoSend className="lg:hidden" />
-                        </Button>
-                    </form>
-                </div>
+          ))}
+        </div>
+        {/* Chat Window */}
+        <div
+          className="flex-1 p-4 overflow-y-auto flex flex-col min-h-[300px] max-h-[500px]"
+          ref={chatContainerRef}
+        >
+          {messages.map((message, index) => (
+            <div
+              key={index}
+              className={`my-2 max-w-[250px] lg:max-w-[500px] xl:max-w-[550px] p-2 rounded-md ${
+                message.role === "user"
+                  ? "bg-blue-100 self-end"
+                  : "bg-gray-100 self-start"
+              }`}
+              ref={index === messages.length - 1 ? lastMessageRef : null}
+            >
+              <ReactMarkdown>{message.content}</ReactMarkdown>
             </div>
-        </section>
-    );
+          ))}
+          {/* Fall back for empty chat window */}
+          <div className="flex-1 min-h-[60vh] w-full flex items-center justify-center">
+            {messages.length === 0 && (
+              <div className="flex flex-col space-y-4 items-center">
+                <RiRobot2Line className="w-[24px] h-[24px]" />
+                <p>Send a message to start chatting with my portfolio</p>
+                <p>Feel free to use the above suggestions</p>
+              </div>
+            )}
+          </div>
+        </div>
+        {/* Input Field goes here . . . */}
+        <div className="p-4 border-t border-gray-200">
+          <form className="flex items-center space-x-2" onSubmit={handleSubmit}>
+            <Input
+              type="text"
+              value={input}
+              onChange={handleInputChange}
+              className="flex-1 border border-gray-300 p-2 rounded-md"
+              placeholder="Type a message..."
+            />
+            <Button className="rounded-md flex items-center" type="submit">
+              <span className="hidden lg:flex">Send</span>
+              <IoSend className="lg:hidden" />
+            </Button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Chat;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,145 +1,152 @@
 import Ripple from "@/components/ui/ripple";
 import Image from "next/image";
-import {BackgroundGradientAnimation} from "@/components/ui/gradient-animation";
+import { BackgroundGradientAnimation } from "@/components/ui/gradient-animation";
 import Link from "next/link";
-import {icons, socialMedia} from "@/data";
-import {Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter} from "@/components/ui/card";
-import {Button} from "@/components/ui/button";
-import meem from "../../public/meeem.png"
+import { icons, socialMedia } from "@/data";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import meem from "../../public/meeem.png";
 
 export default function Index() {
   return (
-      <div className="max-w-7xl w-full p-4 md:p-8 mx-auto">
-          {/*  //! HERO - I  */}
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-              <div
-                  className="col-span-1 lg:col-span-2 h-full bg-gray-200/60 min-h-[500px] lg:min-h-[300px] rounded-2xl p-8">
-                  <h1 className="text-3xl lg:text-6xl font-medium">Hey I am Adarsh ⭐</h1>
-                  <h1 className="text-muted-foreground lg:text-lg mt-8">
-                      A passionate self-learner and pre-final year student with a strong
-                      interest in continuously up-skilling. Dedicated to applying acquired
-                      knowledge and skills to tackle real-world challenges and create
-                      impactful solutions
-                  </h1>
+    <div className="max-w-7xl w-full p-4 md:p-8 mx-auto">
+      {/*  //! HERO - I  */}
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        <div className="col-span-1 lg:col-span-2 h-full bg-gray-200/60 min-h-[500px] lg:min-h-[300px] rounded-2xl p-8">
+          <h1 className="text-3xl lg:text-6xl font-medium">
+            Hey I am Adarsh ⭐
+          </h1>
+          <h1 className="text-muted-foreground lg:text-lg mt-8">
+            A passionate self-learner and pre-final year student with a strong
+            interest in continuously up-skilling. Dedicated to applying acquired
+            knowledge and skills to tackle real-world challenges and create
+            impactful solutions
+          </h1>
 
-                  {/* // ! add mailto: here */}
-                  <a href="#" className="relative inline-block text-lg group mt-8">
-          <span
-              className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
-            <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+          {/* // ! add mailto: here */}
+          <a href="#" className="relative inline-block text-lg group mt-8">
+            <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+              <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+              <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+              <span className="relative">Get in Touch!</span>
+            </span>
             <span
-                className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
-            <span className="relative">Get in Touch!</span>
-          </span>
-                      <span
-                          className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
-                          data-rounded="rounded-lg"
-                      ></span>
-                  </a>
-              </div>
+              className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+              data-rounded="rounded-lg"
+            ></span>
+          </a>
+        </div>
 
-              <div className="relative col-span-1 object-cover rounded-2xl bg-gray-200/60">
-                  <div
-                      className="relative flex h-full w-full max-w-[32rem] items-center justify-center overflow-hidden rounded-lg border bg-background p-20 md:shadow-xl">
-                      <p className="z-10 whitespace-pre-wrap text-center text-5xl font-medium tracking-tighter text-white">
-                          Welcome
-                      </p>
-                      <Ripple/>
-                  </div>
-              </div>
+        <div className="relative col-span-1 object-cover rounded-2xl bg-gray-200/60">
+          <div className="relative flex h-full w-full max-w-[32rem] items-center justify-center overflow-hidden rounded-lg border bg-background p-20 md:shadow-xl">
+            <p className="z-10 whitespace-pre-wrap text-center text-5xl font-medium tracking-tighter text-white">
+              Welcome
+            </p>
+            <Ripple />
           </div>
-
-          {/*  //! HERO - II  */}
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-10">
-              <div className="w-full relative col-span-1 h-full">
-                  <BackgroundGradientAnimation>
-                      <div
-                          className="absolute inset-0 flex items-center justify-center text-white font-bold px-4 text-2xl text-center md:text-3xl lg:text-5xl z-50">
-                          <Link
-                              className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20"
-                              href="chat"
-                          >
-                              Chat With My Portfolio
-                          </Link>
-                      </div>
-                  </BackgroundGradientAnimation>
-              </div>
-              <div className="flex flex-col w-full col-span-1 lg:col-span-2 gap-4">
-                  <Card className="bg-gray-200/60 border-none">
-                      <CardHeader>
-                          <CardTitle>Explore my stack</CardTitle>
-                          <CardDescription>Check out the tools I use daily</CardDescription>
-                      </CardHeader>
-                      <CardContent className="flex flex-wrap gap-4">
-                          {icons.map((item, index) => (
-                              <Image key={index} src={item} alt="Icon" className="w-16 h-16"/>
-                          ))}
-                      </CardContent>
-                  </Card>
-
-                  <div className="grid grid-cols-1 sm:grid-cols-3 w-full gap-4">
-                      {socialMedia.map((item) => (
-                          <Card
-                              key={item.id}
-                              className="p-4 flex flex-col lg:items-center sm:items-start bg-gray-200/60 border-none"
-                          >
-                              <Image
-                                  src={item.icon}
-                                  alt="Icon"
-                                  width={64}
-                                  height={64}
-                                  className="w-16 h-16"
-                              />
-                              <h1 className="text-2xl font-medium pt-3">{item.name}</h1>
-                              <p className="text-muted-foreground">{item.username}</p>
-                              <Button className="mt-4" size="sm" asChild>
-                                  <a href={item.link}>Follow</a>
-                              </Button>
-                          </Card>
-                      ))}
-                  </div>
-              </div>
-          </div>
-
-          {/*  //! HERO - III  */}
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-10">
-              <Card className="col-span-1 lg:col-span-2 border-none bg-gray-200/60 p-10 flex flex-col">
-                  <h1 className="text-3xl lg:text-6xl">
-                      Dedicated Pre-final Year Student and Aspiring Web Developer
-                  </h1>
-                  <p className="mt-8 text-muted-foreground lg:text-lg">
-                      I am a pre-final year college student with a passion for web
-                      development. I enjoy learning new technologies and frameworks, and
-                      I&apos;m dedicated to creating efficient and user-friendly web
-                      applications. I aim to solve real-world problems through innovative
-                      solutions and continuously improve my skills to stay up-to-date in the
-                      ever-evolving tech landscape.
-                  </p>
-
-                  <a
-                      href="mailto:"
-                      className="relative inline-block text-lg group mt-8 w-fit"
-                  >
-          <span
-              className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
-            <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-            <span
-                className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
-            <span className="relative">Get in Touch!</span>
-          </span>
-                      <span
-                          className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
-                          data-rounded="rounded-lg"
-                      ></span>
-                  </a>
-              </Card>
-
-              <div className="col-span-1 flex">
-                  <div className="w-full bg-gray-200/60 flex items-center justify-center rounded-lg">
-                      <Image src={meem} alt="Profile Picture" width={300} height={300}/>
-                  </div>
-              </div>
-          </div>
+        </div>
       </div>
-  )
+
+      {/*  //! HERO - II  */}
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-10">
+        <div className="w-full relative col-span-1 h-full">
+          <BackgroundGradientAnimation>
+            <div className="absolute inset-0 flex items-center justify-center text-white font-bold px-4 text-2xl text-center md:text-3xl lg:text-5xl z-50">
+              <Link
+                className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20"
+                href="chat"
+              >
+                Chat With My Portfolio
+              </Link>
+            </div>
+          </BackgroundGradientAnimation>
+        </div>
+        <div className="flex flex-col w-full col-span-1 lg:col-span-2 gap-4">
+          <Card className="bg-gray-200/60 border-none">
+            <CardHeader>
+              <CardTitle>Explore my stack</CardTitle>
+              <CardDescription>Check out the tools I use daily</CardDescription>
+            </CardHeader>
+            <CardContent className="flex flex-wrap gap-4">
+              {icons.map((item, index) => (
+                <Image
+                  key={index}
+                  src={item}
+                  alt="Icon"
+                  className="w-16 h-16"
+                />
+              ))}
+            </CardContent>
+          </Card>
+
+          <div className="grid grid-cols-1 sm:grid-cols-3 w-full gap-4">
+            {socialMedia.map((item) => (
+              <Card
+                key={item.id}
+                className="p-4 flex flex-col lg:items-center sm:items-start bg-gray-200/60 border-none"
+              >
+                <Image
+                  src={item.icon}
+                  alt="Icon"
+                  width={64}
+                  height={64}
+                  className="w-16 h-16"
+                />
+                <h1 className="text-2xl font-medium pt-3">{item.name}</h1>
+                <p className="text-muted-foreground">{item.username}</p>
+                <Button className="mt-4" size="sm" asChild>
+                  <a href={item.link}>Follow</a>
+                </Button>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {/*  //! HERO - III  */}
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-10">
+        <Card className="col-span-1 lg:col-span-2 border-none bg-gray-200/60 p-10 flex flex-col">
+          <h1 className="text-3xl lg:text-6xl">
+            Dedicated Pre-final Year Student and Aspiring Web Developer
+          </h1>
+          <p className="mt-8 text-muted-foreground lg:text-lg">
+            I am a pre-final year college student with a passion for web
+            development. I enjoy learning new technologies and frameworks, and
+            I&apos;m dedicated to creating efficient and user-friendly web
+            applications. I aim to solve real-world problems through innovative
+            solutions and continuously improve my skills to stay up-to-date in
+            the ever-evolving tech landscape.
+          </p>
+
+          <a
+            href="mailto:"
+            className="relative inline-block text-lg group mt-8 w-fit"
+          >
+            <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+              <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+              <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+              <span className="relative">Get in Touch!</span>
+            </span>
+            <span
+              className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+              data-rounded="rounded-lg"
+            ></span>
+          </a>
+        </Card>
+
+        <div className="col-span-1 flex">
+          <div className="w-full bg-gray-200/60 flex items-center justify-center rounded-lg">
+            <Image src={meem} alt="Profile Picture" width={300} height={300} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
